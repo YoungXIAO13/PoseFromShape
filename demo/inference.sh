17697d65ed9d9489156858b1a21b8f580491e50c
Yang <appledude1994@gmail.com>
2020-04-30 10:33:17 +0200

    Update inference.sh

--- demo/inference.sh
+++ demo/inference.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=1 python inference.py --num_render=12 --shape='MultiView' \
+python inference.py --num_render=12 --shape='MultiView' \
 --model='model/ObjectNet3D.pth' \
 --image_path='demo/armadillo_images' \
 --render_path='demo/armadillo_multiviews/crop' \
