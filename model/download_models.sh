490318620354ab5306c8f37439ccb6c7918953bc
Yang <appledude1994@gmail.com>
2021-05-21 09:31:43 +0200

    Update download_models.sh

--- model/download_models.sh
+++ model/download_models.sh
@@ -1,9 +1,14 @@
-wget https://dl.dropboxusercontent.com/s/knp261x9170nagv/Pascal3D.pth?dl=0 && mv Pascal3D.pth?dl=0 Pascal3D.pth
 
-wget https://dl.dropboxusercontent.com/s/0cn9ktzw215jmmx/ObjectNet3D.pth?dl=0 && mv ObjectNet3D.pth?dl=0 ObjectNet3D.pth
+# wget https://dl.dropboxusercontent.com/s/knp261x9170nagv/Pascal3D.pth?dl=0 && mv Pascal3D.pth?dl=0 Pascal3D.pth
+# wget https://dl.dropboxusercontent.com/s/0cn9ktzw215jmmx/ObjectNet3D.pth?dl=0 && mv ObjectNet3D.pth?dl=0 ObjectNet3D.pth
+# wget https://dl.dropboxusercontent.com/s/kmy54puhvblun8y/ObjectNet3D_novel.pth?dl=0 && mv ObjectNet3D_novel.pth?dl=0 ObjectNet3D_novel.pth
+# wget https://dl.dropboxusercontent.com/s/j0gfqeaje3rxhtz/ShapeNetCore.pth?dl=0 && mv ShapeNetCore.pth?dl=0 ShapeNetCore.pth
+# wget https://dl.dropboxusercontent.com/s/ylbljf5zpkqs9tz/ObjectNet3D_novel_pointcloud.pth?dl=0 && mv ObjectNet3D_novel_pointcloud.pth?dl=0 ObjectNet3D_novel_pointcloud.pth
 
-wget https://dl.dropboxusercontent.com/s/kmy54puhvblun8y/ObjectNet3D_novel.pth?dl=0 && mv ObjectNet3D_novel.pth?dl=0 ObjectNet3D_novel.pth
 
-wget https://dl.dropboxusercontent.com/s/j0gfqeaje3rxhtz/ShapeNetCore.pth?dl=0 && mv ShapeNetCore.pth?dl=0 ShapeNetCore.pth
-
-wget https://dl.dropboxusercontent.com/s/ylbljf5zpkqs9tz/ObjectNet3D_novel_pointcloud.pth?dl=0 && mv ObjectNet3D_novel_pointcloud.pth?dl=0 ObjectNet3D_novel_pointcloud.pth
+## wget with previous links may fail, please try the following
+wget https://www.dropbox.com/s/j0gfqeaje3rxhtz/ShapeNetCore.pth?dl=0 && mv ShapeNetCore.pth?dl=0 ShapeNetCore.pth
+wget https://www.dropbox.com/s/knp261x9170nagv/Pascal3D.pth?dl=0 && mv Pascal3D.pth?dl=0 Pascal3D.pth
+wget https://www.dropbox.com/s/0cn9ktzw215jmmx/ObjectNet3D.pth?dl=0 && mv ObjectNet3D.pth?dl=0 ObjectNet3D.pth
+wget https://www.dropbox.com/s/kmy54puhvblun8y/ObjectNet3D_novel.pth?dl=0 && mv ObjectNet3D_novel.pth?dl=0 ObjectNet3D_novel.pth
+wget https://www.dropbox.com/s/ylbljf5zpkqs9tz/ObjectNet3D_novel_pointcloud.pth?dl=0 && mv ObjectNet3D_novel_pointcloud.pth?dl=0 ObjectNet3D_novel_pointcloud.pth
